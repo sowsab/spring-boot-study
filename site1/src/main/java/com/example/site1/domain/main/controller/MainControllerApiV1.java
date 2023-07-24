febcd230a13a92438ff161ad4db39c01dce70798
sowsab <sowsab@naver.com>
2023-07-24 16:58:36 +0900

    study

--- site1/src/main/java/com/example/site1/domain/main/controller/MainControllerApiV1.java
+++ site1/src/main/java/com/example/site1/domain/main/controller/MainControllerApiV1.java
@@ -1,17 +1,24 @@
 package com.example.site1.domain.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.site1.domain.main.service.MainServiceApiV1;
 
 @RestController
 @RequestMapping("/api/v1/main")
 public class MainControllerApiV1 {
     
-    
+    @Autowired
+    private MainServiceApiV1 mainServiceApiV1;
 
     @GetMapping
-    public void getUserList() {
+    public ResponseEntity<?> getUserList() {
+
+        return mainServiceApiV1.getUserList();
 
     }
 
