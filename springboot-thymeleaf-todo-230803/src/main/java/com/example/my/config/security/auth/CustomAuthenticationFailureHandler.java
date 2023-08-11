5bd0bf155964dfd576139d784c6d8830f44a5a5e
sowsab <sowsab@naver.com>
2023-08-11 09:30:13 +0900

    study

--- springboot-thymeleaf-todo-230803/src/main/java/com/example/my/config/security/auth/CustomAuthenticationFailureHandler.java
+++ springboot-thymeleaf-todo-230803/src/main/java/com/example/my/config/security/auth/CustomAuthenticationFailureHandler.java
@@ -6,18 +6,36 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
+import com.example.my.common.dto.ResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException exception) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        ResponseDTO<Object> responseDTO = ResponseDTO.builder()
+        .code(1)
+        .message("로그인에 실패했습니다")
+        .build();
+
+        String body = objectMapper.writeValueAsString(responseDTO);
+
+        response.getWriter().print(body);
     }
     
 }
--- springboot-thymeleaf-todo-230803/src/main/java/com/example/my/config/security/auth/CustomAuthenticationSuccessHandler.java
+++ springboot-thymeleaf-todo-230803/src/main/java/com/example/my/config/security/auth/CustomAuthenticationSuccessHandler.java
@@ -6,18 +6,40 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.example.my.common.dto.ResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    // 객체를 json으로 변환해줄 코드
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+        // 옛날에 쓰던 자바 코드 그대로 사용 한다나 뭐라나
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setCharacterEncoding("UTF-8");
+        // api를 쓰기로 해서 json 사용한다 선언
+        response.setContentType("application/json");
+
+        ResponseDTO<Object> responseDTO = ResponseDTO.builder()
+        .code(0)
+        .message("로그인에 성공했습니다")
+        .build();
+
+        String body = objectMapper.writeValueAsString(responseDTO);
+
+        response.getWriter().print(body);
+
+
     }
     
 }
